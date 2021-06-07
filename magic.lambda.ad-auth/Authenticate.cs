29f0686e40e89c476253136e42c67988e7a8859c
Thomas Hansen <thomas@gaiasoul.com>
2021-06-07 14:58:07 +0300

    Initial commit

---
+++ magic.lambda.ad-auth/Authenticate.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - 2021, thomas@servergardens.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
+ */
+
+using System.Linq;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.lambda.sockets
+{
+    /// <summary>
+    /// [ad-auth.authenticate] slot that authenticates a user over Active Directory.
+    /// </summary>
+    [Slot(Name = "ad-auth.authenticate")]
+    public class Authenticate : ISlot
+    {
+        /// <summary>
+        /// Slot implementation.
+        /// </summary>
+        /// <param name="signaler">Signaler that raised signal.</param>
+        /// <param name="input">Arguments to slot.</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+        }
+    }
+}
